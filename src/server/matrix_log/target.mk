49dcafd774221667d80b852caf754123a12f807f
Johannes Kliemann <jk@jkliemann.de>
2018-07-04 09:40:56 +0200

    fix matrix_log target

--- src/server/matrix_log/target.mk
+++ src/server/matrix_log/target.mk
@@ -1,7 +1,5 @@
 TARGET = matrix_log
 SRC_CC = main.cc gnat.cc
-SRC_ADB = client.adb http.adb
+SRC_ADB = client.adb
 LIBS = ada base amatrix jwx
 INC_DIR += $(PRG_DIR)
-
-vpath http.adb $(REP_DIR)/src/lib/amatrix
