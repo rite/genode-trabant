6812055fdbd59228f9810bf54692a76c265b50d0
Roman Iten <roman.iten@gapfruit.com>
2018-10-11 14:05:41 +0200

    vga_log: add include directory

--- src/drivers/vga_log/target.mk
+++ src/drivers/vga_log/target.mk
@@ -2,6 +2,7 @@ TARGET   = vga_log_drv
 LIBS     = base ada
 SRC_CC   = main.cc
 SRC_ADB  = vga.adb escape_dfa.adb
-INC_DIR += $(PRG_DIR)/include
+INC_DIR += $(PRG_DIR) \
+           $(PRG_DIR)/include
 
 include $(REP_DIR)/mk/gnat_opts.mk
